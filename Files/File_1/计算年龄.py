0762572966bff0a0112b34ec7dd3993e4340aa22
Bowen-0906 <Bowen_0906@163.com>
2022-07-19 17:10:43 +0800

    Update 计算年龄.py

--- Files/File_1/计算年龄.py
+++ Files/File_1/计算年龄.py
@@ -3,8 +3,8 @@ year = int(input("请输入你的出生年："))
 month = int(input("请输入你的出生月："))
 now_year = int(strftime("%Y"))
 now_month = int(strftime("%m"))
-month2 = now_month - month
-if month2 < 0:
+month_difference = now_month - month
+if month_difference < 0:
     your_age = now_year - year - 1
 else:
     your_age = now_year - year
